78d27d32fe79a2dd247be086d31695bf4292d51c
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-07-14 22:58:43 -0400

    removed venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/lukemackenzie/Documents/learning/python/pollster-django/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
